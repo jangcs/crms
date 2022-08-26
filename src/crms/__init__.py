32c95609bf883f0c452e46fe07201b4232fe9c54
jangcs <jangcs@gmail.com>
2022-08-26 17:41:44 +0900

    package funcs

--- src/crms/__init__.py
+++ src/crms/__init__.py
@@ -2,6 +2,11 @@
 #from .crms import arg_parse
 from .crms import crms_conf_api
 from .crms import crms_conf_mod_api
+from .crms import crms_init_api
+from .crms import crms_add_api
+from .crms import crms_push_api
+from .crms import crms_pull_api
 from .crms import crms_list_api
+from .crms import crms_desc_api
 
 ### How to make package - reference : https://mmjourney.tistory.com/14
