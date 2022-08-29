cf3bc66a7833ae32b6a372ba77e3e38381302843
Choulsoo Jang from CR-EDGE <jangcs@gmail.com>
2022-08-29 09:51:14 +0900

    .

--- example1.py
+++ example1.py
@@ -2,5 +2,5 @@ import crms
 
 #args = crms.arg_parse()
 #crms.crms(args)
-crms.crms_list_api()
-crms.crms_conf_api("git@github.com:jangcs/model_t.git", "gs://cr-model/model_t")
+crms.crms_list()
+crms.crms_conf("git@github.com:jangcs/model_t.git", "gs://cr-model/model_t")
--- example2.py
+++ example2.py
@@ -2,5 +2,5 @@ import crms
 
 #args = crms.arg_parse()
 #crms.crms(args)
-crms.crms_list_api()
-crms.crms_conf_mod_api("mod@github.com:jangcs/model_t.git", "gs://cr-model_mod/model_t")
+crms.crms_list()
+crms.crms_conf_mod("mod@github.com:jangcs/model_t.git", "gs://cr-model_mod/model_t")
