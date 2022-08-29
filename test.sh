3f856af25de8ce7a4bee7175e9ec40e79b38c9bc
jangcs <jangcs@gmail.com>
2022-08-29 11:03:07 +0900

    test -------------------

--- test.sh
+++ test.sh
@@ -11,10 +11,9 @@ then
 	exit 1
 fi
 
+echo "------------------------------------------------------------------------------------------"
 echo "* Not exist. Make dir(""$dir"")" 
 mkdir $dir
-
-
 
 echo "* Change working directory to" $dir
 cd $dir
@@ -22,48 +21,60 @@ echo "* Working directory is" "`pwd`"
 
 wd="`pwd`"
 
+echo "------------------------------------------------------------------------------------------"
 echo "* Copy a model for test(m1.pth)"
 cp ../src/crms/other_data/m1.pth $wd
 
 
+echo "------------------------------------------------------------------------------------------"
 echo "* CRMS config"
 #../crms conf git@github.com:jangcs/"$dir".git gs://cr-model-test/models/$dir
 crms conf git@github.com:jangcs/"$dir".git gs://cr-model/$dir
 
+echo "------------------------------------------------------------------------------------------"
 echo "* CRMS init with model name(""$dir"")"
 crms init $dir
 
 
+echo "------------------------------------------------------------------------------------------"
 echo "* CRMS add a model file"
 crms add m1.pth
 
 
+echo "------------------------------------------------------------------------------------------"
 echo "* CRMS push (version 0.1)"
 crms push v0.1 
 
 
+echo "------------------------------------------------------------------------------------------"
 echo "* CRMS list"
 crms list
 
+echo "------------------------------------------------------------------------------------------"
 echo "* CRMS desc a model"
 crms desc $dir
 
 
 
+echo "------------------------------------------------------------------------------------------"
 echo "* Copy a model2 for test(m2.pth)"
 cp ../src/crms/other_data/m2.pth $wd
 
 
+echo "------------------------------------------------------------------------------------------"
 echo "* CRMS add a model file"
 crms add m2.pth
 
 
+echo "------------------------------------------------------------------------------------------"
 echo "* CRMS push (version 0.2)"
 crms push v0.2 
 
 
+echo "------------------------------------------------------------------------------------------"
 echo "* CRMS list"
 crms list
 
+echo "------------------------------------------------------------------------------------------"
 echo "* CRMS desc a model"
 crms desc $dir
