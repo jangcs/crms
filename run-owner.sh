cd2307d73f2eda4b7037fdf15eea554cb7d6c8cd
Choulsoo Jang from CR-EDGE <jangcs@gmail.com>
2023-04-14 15:59:05 +0900

    sh for docker

--- run-owner.sh
+++ run-owner.sh
@@ -5,5 +5,5 @@ docker run --interactive \
            --volume "$(pwd)"/models:/models \
            --mount type=bind,source="$(pwd)"/cloudrobotai-owner-e5ca3becb1d7.json,target=/app/cloudrobotai-owner-e5ca3becb1d7.json \
            --env GOOGLE_APPLICATION_CREDENTIALS=/app/cloudrobotai-owner-e5ca3becb1d7.json \
-           crms:0.1 \
+           crms-agent:0.9 \
            /usr/local/bin/crms pull git@github.com:jangcs/model_v4.git -t /models/model_v4 
--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ docker run --interactive \
            --rm \
            --name crms \
            --volume "$(pwd)"/models:/models \
-           crms:0.1 \
+           crms-agent:0.9 \
            /usr/local/bin/crms pull git@github.com:jangcs/model_v4.git -t /models/model_v4 
