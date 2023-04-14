e2a69ad96c8169d27b8bdfde2b5fc0b19f26c3a0
Choulsoo Jang from CR-EDGE <jangcs@gmail.com>
2023-04-14 15:48:08 +0900

    run-watchdog.sh

--- run-watchdog.sh
+++ run-watchdog.sh
@@ -2,7 +2,7 @@ docker run --interactive \
            --tty \
            --rm \
            --name crms \
-           --volume "$(pwd)"/models:/models \
+           --volume "$(pwd)"/crms-models:/crms-models \
            --mount type=bind,source="$(pwd)"/watchdog.env,target=/app/.env \
-           -p 5000:5000 \
+           -p 6537:6537 \
            crms:0.1 
